249a038517b0fa1d6d1e71db77cf2c62294b268f
Wasay Siddiqui <wasaysiddiqui379@gmail.com>
2025-04-21 00:58:41 -0400

    in the process of implementing commented features

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -6,16 +6,37 @@ public class Vehicle {
     private double topSpeed;
     private double stallSpeed;
 
+    public Vehicle(String[] manufacturers, int birthYear, int outOfServiceYear, double topSpeed, double stallSpeed) {
+        this.manufacturers = manufacturers;
+        this.birthYear = birthYear;
+        this.outOfServiceYear = outOfServiceYear;
+        this.topSpeed = topSpeed;
+        this.stallSpeed = stallSpeed;
+    }
+
+    public String[] getManufacturers(String[] contracters) {
+        return manufacturers;
+    }
+
 }
 
 /*list of instance methods to implement:
- - public String[] getManufacturers()
-also make a method to see if any two vehicles were worked on by the same company, public String[] commonManufacturers()
+ - public String[] getManufacturers() {}
+also make a method to see if any two vehicles were worked on by the same company: 
+
+public String[] commonManufacturers()
 
  - public int YearsInService()
-outOfServiceYear - birthyear
+return outOfServiceYear - birthyear;
+
+ - public void getBriefHistory()
+System.out.print("give a brief history of the selcted aircraft, maybe talk about the time period of manufacturing")
+
+ - 
 
  - public int TopSpeedDifference()
  - public int StallSpeedDifference()
+
+ make methods that return the value of each instance variable, getBirthYear(), getDecommissionYear(), getTopSpeed(), getStallSpeed() 
 */ 
 
