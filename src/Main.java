af31e15eaadedf1fcba1f37fec7d05689f54d0cd
Wasay Siddiqui <wasaysiddiqui379@gmail.com>
2025-04-22 01:57:17 -0400

    small error present with getInstanceVariable() methods - fix for later

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,52 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
-        
-        final int LIST_NUMBER = 2;
+
+        final int LIST_NUMBER = 8;
 
         System.out.println("Hello operater!");
-        System.out.println("This is a list, in no particular order, of aircraft that the author of this program likes/knows about :)");
-        System.out.println("Please enter the index of the specific vehicle you are looking for, starting from 1 to" + LIST_NUMBER);
+        System.out.println(
+                "This is a list, in no particular order, of military aircraft that the author of this program likes/knows about :)");
+        System.out.println("Please enter the index of the specific vehicle you are looking for, starting from 1 to "
+                + LIST_NUMBER + ".");
         System.out.println("you may also select another aircraft to compare between the two!");
 
-        for(int i = 0; i < LIST_NUMBER; i++) {
+        for (int i = 0; i < LIST_NUMBER; i++) {
             System.out.println(Vehicle.values()[i]);
         }
 
-        int userInput = scan.nextInt();
+        int userInput = scan.nextInt(); // original selected vehicle
+
+        System.out.println(" -> Here is brief history about the vehicle to start off:");
+        Vehicle.values()[userInput - 1].getBriefHistory();
+
+        System.out.println(" -> Next, are some of the manufacturers who worked on this vehicle:");
+        Vehicle.values()[userInput - 1].getManufacturers();
+
+        System.out.println(" -> Year the vehicle was put into production:");
+        Vehicle.values()[userInput - 1].getBirthYear();
+
+        System.out.println(" -> Year the vehicle was retired/taken out of production:");
+        Vehicle.values()[userInput - 1].getDecommissionYear();
+
+        System.out.println(" -> How many years the vehicle was in use/production:");
+        Vehicle.values()[userInput - 1].YearsInService();
+
+        System.out.println(" -> Top seepd in knots:");
+        Vehicle.values()[userInput - 1].getTopSpeed();
+
+        System.out.println(" -> Stall speed in knots:");
+        Vehicle.values()[userInput - 1].getStallSpeed();
+
+        System.out.println("now please enter the aircraft you would like to compare statistics to!");
+        int inputSecondAircraft = scan.nextInt(); // comparison vehicle
 
         scan.close();
 
-        Vehicle.values()[userInput - 1].getBriefHistory();
-        Vehicle.values()[userInput - 1].getManufacturers();
-        Vehicle.values()[userInput - 1].commonManufacturers(Vehicle.values()[1]);
+        System.out.println(" -> Here is a list of common manufacturers between the two vehicles selected:");
+        Vehicle.values()[userInput - 1].commonManufacturers(Vehicle.values()[inputSecondAircraft - 1]);
 
-        
     }
 }
-
-
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,13 +1,18 @@
 public enum Vehicle {
-    //String[] A10Manuf = new String["Fairchild", "Amazon", "Pepsi"];
 
-    A10(new String[]{"Fairchild", "General Electric", "Grumman"}, 1977, 2025, 381, 120),
-    F14(new String[]{"Grumman", "Pratt-Whitney", "Hughes"}, 1969, 2006, 381, 120);
-    
+    A10(new String[] { "Fairchild", "General Electric", "Grumman" }, 1977, 2025, 381, 120),
+    F16(new String[] { "General Dynamics", "Pratt-Whitney", "Lockheed Martin" }, 1976, 2025, 800, 140),
+    F18(new String[] { "McDonnell-Douglas", "General Electric", "Grumman" }, 1977, 2025, 381, 120),
+    ARROW(new String[] { "Avro", "General Electric", "Grumman" }, 1977, 2025, 381, 120),
+    SU35(new String[] { "Sukhoi", "General Electric", "Grumman" }, 1977, 2025, 381, 120),
+    MIG29(new String[] { "Mikoyan-Gurevich", "General Electric", "Grumman" }, 1977, 2025, 381, 120),
+    B52(new String[] { "General Dynamics", "General Electric", "Grumman" }, 1977, 2025, 381, 120),
+    F14(new String[] { "Grumman", "Pratt-Whitney", "Hughes" }, 1969, 2006, 381, 120);
+
     private String[] manufacturer;
     private int birthYear;
     private int outOfServiceYear;
-    private int topSpeed; //all speeds in knots
+    private int topSpeed; // all speeds in knots
     private int stallSpeed;
 
     private Vehicle(String[] manufacturer, int birthYear, int outOfServiceYear, int topSpeed, int stallSpeed) {
@@ -19,7 +24,7 @@ public enum Vehicle {
     }
 
     public String[] getManufacturers() {
-        for(int a = 0; a < manufacturer.length; a++) {
+        for (int a = 0; a < manufacturer.length; a++) {
             System.out.println(manufacturer[a]);
         }
         return manufacturer;
@@ -47,49 +52,49 @@ public enum Vehicle {
 
     public void getBriefHistory() {
 
-        switch(this.ordinal()) {
+        switch (this.ordinal()) {
             case 0:
                 System.out.println("GAU-8 Avenger go brrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrtttttttttttttttttttttttttttt");
-            break;
+                break;
+            case 1:
+                System.out.println("Maverick make wing go sweeeeeeeeeeeeeeep");
+                break;
 
             default:
-                System.out.print("give a brief history of the selcted aircraft, maybe talk about the time period of manufacturing");
+                System.out.print("No history, vehicle does not exist within enumerated array bounds.");
         }
-        
-        }
+
+    }
 
     public void commonManufacturers(Vehicle b) {
 
         for (int i = 0; i < this.manufacturer.length; i++) {
             for (int j = 0; j < b.manufacturer.length; j++) {
                 if (this.manufacturer[i].equals(b.manufacturer[j])) {
-
                     System.out.println(b.manufacturer[j]);
                 }
             }
         }
     }
-
 }
 
-/*list of instance methods to implement:
- - public String[] getManufacturers() {}
-also make a method to see if any two vehicles were worked on by the same company: 
-
-
-
- - public int YearsInService()
-return outOfServiceYear - birthyear;
-
- - public void getBriefHistory() {
-System.out.print("give a brief history of the selcted aircraft, maybe talk about the time period of manufacturing")
-}
-
- - 
-
- - public int TopSpeedDifference()
- - public int StallSpeedDifference()
-
- make methods that return the value of each instance variable, getBirthYear(), getDecommissionYear(), getTopSpeed(), getStallSpeed() 
-*/ 
-
+/*
+ * list of instance methods to implement:
+ * - public String[] getManufacturers() {}
+ * also make a method to see if any two vehicles were worked on by the same
+ * company:
+ * 
+ * 
+ * 
+ * - public int YearsInService()
+ * return outOfServiceYear - birthyear;
+ * 
+ * - public void getBriefHistory() {
+ * System.out.
+ * print("give a brief history of the selcted aircraft, maybe talk about the time period of manufacturing"
+ * )
+ * }
+ * 
+ * make methods that return the value of each instance variable, getBirthYear(),
+ * getDecommissionYear(), getTopSpeed(), getStallSpeed()
+ */
