4c1d7f79f9c52a0667f6d43c465b66b6c5ced21d
Wasay Siddiqui <wasaysiddiqui379@gmail.com>
2025-04-23 01:28:02 -0400

    Finished a working aircraft comparison program

--- src/Main.java
+++ src/Main.java
@@ -19,13 +19,10 @@ public class Main {
 
         int userInput = scan.nextInt(); // original selected vehicle
 
-        System.out.println(" -> Here is brief history about the vehicle to start off:");
-        Vehicle.values()[userInput - 1].getBriefHistory();
-
-        System.out.println(" -> Next, are some of the manufacturers who worked on this vehicle:");
+        System.out.println(" -> First of all, here are some of the manufacturers/contractors who worked on this vehicle:");
         Vehicle.values()[userInput - 1].getManufacturers();
 
-        System.out.println(" -> Year the vehicle was put into production:");
+        System.out.println(" -> Year the vehicle was casted as an idea:");
         System.out.println(Vehicle.values()[userInput - 1].getBirthYear());
 
         System.out.println(" -> Year the vehicle was retired/taken out of production:");
@@ -34,19 +31,22 @@ public class Main {
         System.out.println(" -> How many years the vehicle was in use/production:");
         System.out.println(Vehicle.values()[userInput - 1].YearsInService());
 
-        System.out.println(" -> Top seepd in knots:");
+        System.out.println(" -> Top speed in knots:");
         System.out.println(Vehicle.values()[userInput - 1].getTopSpeed());
 
         System.out.println(" -> Stall speed in knots:");
         System.out.println(Vehicle.values()[userInput - 1].getStallSpeed());
 
+        System.out.println(" -> Here is brief history about the vehicle to start off (half the time a funny/true quote about it that I liked):");
+        Vehicle.values()[userInput - 1].getBriefHistory();
+
         System.out.println("now please enter the aircraft you would like to compare statistics to!");
-        int inputSecondAircraft = scan.nextInt(); // comparison vehicle
+        int inputSecondAircraft = scan.nextInt(); // comparison vehicle, sees if there are any common manufacturers only
 
         scan.close();
 
-        System.out.println(" -> Here is a list of common manufacturers between the two vehicles selected:");
-        //System.out.println(Vehicle.values()[userInput - 1].commonManufacturers(Vehicle.values()[inputSecondAircraft - 1]));
+        System.out.println(" -> Here is a list of common manufacturers/contractors between the two vehicles selected:");
+        Vehicle.values()[userInput - 1].commonManufacturers(Vehicle.values()[inputSecondAircraft - 1]);
 
     }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,13 +1,15 @@
 public enum Vehicle {
-
+    
     A10(new String[] { "Fairchild", "General Electric", "Grumman" }, 1977, 2025, 381, 120),
     F16(new String[] { "General Dynamics", "Pratt-Whitney", "Lockheed Martin" }, 1976, 2025, 800, 140),
-    F18(new String[] { "McDonnell-Douglas", "General Electric", "Grumman" }, 1977, 2025, 381, 120),
-    ARROW(new String[] { "Avro", "General Electric", "Grumman" }, 1977, 2025, 381, 120),
-    SU35(new String[] { "Sukhoi", "General Electric", "Grumman" }, 1977, 2025, 381, 120),
-    MIG29(new String[] { "Mikoyan-Gurevich", "General Electric", "Grumman" }, 1977, 2025, 381, 120),
-    B52(new String[] { "General Dynamics", "General Electric", "Grumman" }, 1977, 2025, 381, 120),
-    F14(new String[] { "Grumman", "Pratt-Whitney", "Hughes" }, 1969, 2006, 381, 120);
+    F18(new String[] { "McDonnell-Douglas", "Northrop", "Boeing" }, 1974, 2000, 1067, 135),
+    ARROW(new String[] { "A.V.Roe", "Magellan", "Dowty" }, 1953, 1959, 1300, 170),
+    SU35(new String[] { "Sukhoi", "UAC", "NPO" }, 1988, 2025, 1340, 142),
+    MIG29(new String[] { "Mikoyan-Gurevich", "Klimov", "Avionika" }, 1977, 2025, 1310, 142),
+    B52(new String[] { "Boeing", "Pratt-Whitney", "Northrop Grumman" }, 1952, 2025, 510, 130),
+    F14(new String[] { "Grumman", "Pratt-Whitney", "Hughes" }, 1969, 2006, 381, 120),
+    KA52(new String[] { "Kamov", "Klimov", "KRET" }, 1997, 2025, 167, 0),
+    V22(new String[] { "Bell", "Boeing", "Raytheon" }, 1989, 2025, 275, 0);
 
     private String[] manufacturer;
     private int birthYear;
@@ -31,22 +33,27 @@ public enum Vehicle {
     }
 
     public int getBirthYear() {
+        System.out.println(birthYear);
         return birthYear;
     }
 
     public int getDecommissionYear() {
+        System.out.println(outOfServiceYear);
         return outOfServiceYear;
     }
 
     public int getTopSpeed() {
+        System.out.println(topSpeed);
         return topSpeed;
     }
 
     public int getStallSpeed() {
+        System.out.println(stallSpeed);
         return stallSpeed;
     }
 
     public int YearsInService() {
+        System.out.println(outOfServiceYear - birthYear);
         return outOfServiceYear - birthYear;
     }
 
@@ -57,9 +64,32 @@ public enum Vehicle {
                 System.out.println("GAU-8 Avenger go brrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrtttttttttttttttttttttttttttt");
                 break;
             case 1:
-                System.out.println("Maverick make wing go sweeeeeeeeeeeeeeep");
+                System.out.println("Yo guys, how about we go completely against everything taught in fighter design! - proceeds to make one of the most recognizable fighters ever to serve during the Cold War and cement it's legacy in history as one of the best!");
                 break;
-
+            case 2:
+                System.out.println("US Navy's workhorse jet! has a variant other than the average Hornet called the 'Super Hornet', different enough, thats its considered an entirely new aircraft!");
+                break;
+            case 3:
+                System.out.println("Fly high king (RIP)");
+                break;
+            case 4:
+                System.out.println("'Just make it look like it can actually fight and we won't have a problem!' - some dude at Moscow probably");
+                break;
+            case 5:
+                System.out.println("An aircraft with a feared reputation back then, in fact the F16 was the Western response to it! a workhorse fighter for many Eastern countries, however completely obsolete now due to radar detection distances improving.");
+                break;
+            case 6:
+                System.out.println("Don't fix it if it's not broken!");
+                break;
+            case 7:
+                System.out.println("'I feel the need...the need for speed!'");
+                break;
+            case 8:
+                System.out.println("A Russian attack helicopter with a fearsome reputation before the war in Ukraine, it has a pretty loaded armament with rocket pods like the S-8 and autocannons like the Shipunov 2A42.");
+                break;
+            case 9:
+                System.out.println("Arguably one of the coolest looking aircraft to see fly above your head, it has a tilt-rotor system meaning it can function as both an airplane and a helicopter, switching between them in flight as well! used for fast insertion/extraction of troops in hazardous cconditions.");
+                break;
             default:
                 System.out.print("No history, vehicle does not exist within enumerated array bounds.");
         }
